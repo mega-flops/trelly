473757716b1dd41e5d62b678ba3fb1bee3d546ba
zhelnov <danya.volya@gmail.com>
2026-03-20 19:21:38 +0300

    add header (test)

--- src/ui/MainPage.tsx
+++ src/ui/MainPage.tsx
@@ -2,6 +2,7 @@ import { TaskList } from "./TaskList";
 import { TaskDetails } from "./TaskDetails";
 import { useTaskSelection } from "../bll/useTaskSelection";
 import styles from "./MainPage.module.css";
+import { Header } from "./Header";
 
 export function MainPage() {
   const { selectedTaskId, setSelectedTaskId, boardId, setBoardId } =
@@ -12,6 +13,7 @@ export function MainPage() {
   };
   return (
     <div className={styles.mainpage}>
+      <Header />
       <TaskList onSelect={onSelect} selectedTaskId={selectedTaskId} />
       <TaskDetails selectedTaskId={selectedTaskId} boardId={boardId} />
     </div>
